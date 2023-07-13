001c8802b0da8fb9ac24d0924b8eb91df4ca390a
Kimutai <jankimsingoei@gmail.com>
2023-07-13 08:57:35 +0300

    Done with the task

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(par1,par2){
+    par1=2000;
+    par2=100;
+    return (par1/par2)
+}
+const square =(parameter1) => { return (parameter1 * parameter1)
+}
+const add =(par1 ,par2) =>{
+    return(par1 +par2)
+}
